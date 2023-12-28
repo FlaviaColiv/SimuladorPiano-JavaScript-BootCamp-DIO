7276b680314f953fa01dcd550388620d1c17dcf1
Flavia Oliveira <flaviacoliv@gmail.com>
2023-12-28 01:53:25 -0300

    Ajustando efeitos dos comando no teclado com o JavaScript

--- src/script/engine.js
+++ src/script/engine.js
@@ -5,6 +5,13 @@ let audio = new Audio("src/tunes/a.wav");
 const playTune =(key) => {
     audio.src = `src/tunes/${key}.wav`
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+
+    setTimeout(() => {
+    clickedKey.classList.remove("active");
+    }, 150);
 };
 
 pianoKeys.forEach((key) => {
