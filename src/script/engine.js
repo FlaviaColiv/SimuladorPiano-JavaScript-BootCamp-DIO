28abff54a2d80667082a39e64eac76fb278f2693
Flavia Oliveira <flaviacoliv@gmail.com>
2023-12-28 01:56:30 -0300

    Teclas mapeadas com o JavaScript

--- src/script/engine.js
+++ src/script/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune =(key) => {
@@ -17,9 +18,12 @@ const playTune =(key) => {
 pianoKeys.forEach((key) => {
     console.log(key.dataset.key); 
     key.addEventListener("click", () => playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
 })
 
 document.addEventListener("keydown", 
     (e) => {
-        playTune(e.key)
+        if (mapedKeys.includes(e.key)){
+            playTune(e.key)
+        }
     });
